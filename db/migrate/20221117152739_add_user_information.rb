8d2e1c3e5de184039183d3658fab3b0d3c84614b
Paul <pqy629@gmail.com>
2022-11-19 11:05:13 -0500

    fix migrate

--- db/migrate/20221117152739_add_user_information.rb
+++
@@ -1,9 +0,0 @@
-class AddUserInformation < ActiveRecord::Migration[6.1]
-  def change
-
-    add_column :users, :username, :string
-    add_column :users, :password_digest, :string
-    add_column :users, :employee_id, :integer
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_17_152739) do
+ActiveRecord::Schema.define(version: 2022_11_15_143735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 2022_11_17_152739) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.string "password_digest"
     t.integer "employee_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
